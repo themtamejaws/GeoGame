5c6c930f705ae36d27cb1551415f803dc9d05829
themtamejaws <jamesmatthewmorby@gmail.com>
2015-08-25 17:20:38 +0100

    IT WORKS, kinda...

--- geo-2.py
+++ geo-2.py
@@ -16,12 +16,12 @@ class GeoGame(object):
         click = self.label.bind("<Button-1>", self.callback)
         self.label.pack()
         data = self.load_file()
-        
+        self.gamesetup(data)
 
     def callback(self, event):
         print "clicked at", event.x, event.y
         self.click = [event.x, event.y]
-        self.game()
+        self.gameplay()
 
     def load_file(self):
         f = open('database.txt', 'r').readlines()
@@ -32,13 +32,24 @@ class GeoGame(object):
         return data
 
     def choose_city(self, data):
-        number = random.randint(0,1)
+        citylen = len(data)
+        number = random.randint(0,0)#citylen-1) #maybe not -1
         return data[number]
     
-    def game():
+    def gamesetup(self, data):
         self.city = self.choose_city(data)
 	self.city = [x.strip() for x in self.city.split(',')]
+        print self.city
+	print "Your city to find is: " + self.city[1]
 
+    def gameplay(self):
+        print "well done for clicking on the screen!"
+	print self.click[0], self.city[4]
+
+	if str(self.click[0]) == str(self.city[4]) and str(self.click[1]) == str(self.city[5]):
+	    print "success"
+	else:
+  	    print "you suck"
 
 root = Tk()
 myGeoGame = GeoGame(root)
