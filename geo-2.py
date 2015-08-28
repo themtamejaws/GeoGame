a3812a9439aef61dc2c34e4c9691625cdc4bdae8
Nevyn2345 <sbarnett1@sheffield.ac.uk>
2015-08-28 15:25:44 +0100

    Added target to previous city

--- geo-2.py
+++ geo-2.py
@@ -1,6 +1,6 @@
 from PIL import ImageTk
 from PIL import Image
-from Tkinter import Tk, Label, Button, Frame, Text, END, DISABLED, RIGHT, TOP, BOTTOM, LEFT
+from Tkinter import Tk, Label, Button, Frame, Text, END, DISABLED, RIGHT, TOP, BOTTOM, LEFT, Canvas
 import tkFont
 import random
 import math
@@ -16,14 +16,22 @@ class GeoGame(Frame):
         self.im2 = ImageTk.PhotoImage(self.smallim)
         self.click = []
 
+
+
         self.topFrame = Frame(master)
         self.topFrame.pack(side=TOP)
         self.bottomFrame = Frame(master)
         self.bottomFrame.pack(side=BOTTOM, fill="both")
-        
-        self.label = Label(self.topFrame, image=self.im2)
-        click = self.label.bind("<Button-1>", self.callback)
-        self.label.pack()
+        self.canvas = Canvas(self.topFrame, width = 1200, height=600)
+        self.canvas.pack(expand="yes", fill="both")
+        self.label = self.canvas.create_image(0,0, image=self.im2, anchor='nw')
+        click = self.canvas.bind("<Button-1>", self.callback)
+
+        self.crosshairs = Image.open("crosshairs_small.png")
+        self.cross_width, self.cross_height = self.crosshairs.size
+        self.cross = ImageTk.PhotoImage(self.crosshairs)
+        self.first_round = 0
+       
         self.text = Text(self.bottomFrame, height=1, width=40, font=self.customFont)
         self.text.pack(side=LEFT)
         self.scoreText = Text(self.bottomFrame, height=1,width=10)
@@ -48,11 +56,12 @@ class GeoGame(Frame):
             data.append(line)
         return data
 
+
     def choose_city(self):
         citylen = len(self.data)
         number = citylen+1
-	while number > citylen:
-		number = abs(int(random.gauss(0,100)))
+        while number > citylen:
+            number = abs(int(random.gauss(0,100)))
         return self.data[number]
     
     def gamesetup(self):
@@ -67,12 +76,19 @@ class GeoGame(Frame):
 
 
     def gameplay(self):
+        if self.first_round != 0:
+            self.canvas.delete(self.target)
         self.tot_score += self.distance_score()
         self.scoreText.config(state="normal")
         self.scoreText.delete("1.00", END)
         self.scoreText.insert(END, str(self.tot_score))
         self.scoreText.config(state=DISABLED)
         print "total score = " + str(self.tot_score)
+        print type(self.city[3])
+        self.target = self.canvas.create_image(int(self.city[3])-self.cross_width/2, int(self.city[2])-self.cross_height/2, image=self.cross, anchor='nw')
+        #self.canvas.tag_lower(self.label)
+        print self.city[2], self.city[3]
+        self.first_round = 1
         if str(self.click[0]) == str(self.city[2]) and str(self.click[1]) == str(self.city[3]):
             self.gamesetup()
         else:
